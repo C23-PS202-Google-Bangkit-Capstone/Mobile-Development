7b804bd22a5348485ef58836631551e852ba2a0b
Zahran <zahranvidian@gmail,com>
2023-05-26 14:28:04 +0700

    tflite selesai, tinggal rapihin code.

--- app/src/main/java/com/example/capstoneproject/ui/camera/CameraFragment.kt
+++ app/src/main/java/com/example/capstoneproject/ui/camera/CameraFragment.kt
@@ -7,9 +7,10 @@ import android.content.pm.PackageManager
 import android.graphics.Bitmap
 import android.net.Uri
 import android.os.Bundle
+import android.provider.MediaStore
 import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.Toast
@@ -18,7 +19,14 @@ import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import com.example.capstoneproject.R
+import com.example.capstoneproject.ml.ImageliteModel
 import com.github.dhaval2404.imagepicker.ImagePicker
+import org.tensorflow.lite.DataType
+import org.tensorflow.lite.support.image.ImageProcessor
+import org.tensorflow.lite.support.image.TensorImage
+import org.tensorflow.lite.support.image.ops.ResizeOp
+import org.tensorflow.lite.support.tensorbuffer.TensorBuffer
+
 
 class CameraFragment : Fragment() {
 
@@ -30,25 +38,68 @@ class CameraFragment : Fragment() {
 
     private lateinit var imageView: ImageView
     private lateinit var pickImageButton: Button
-
+    private lateinit var scanButton: Button
     private val viewModel: CameraViewModel by viewModels()
+
+
+    var imageProcessor = ImageProcessor.Builder()
+        .add(ResizeOp(224,224, ResizeOp.ResizeMethod.BILINEAR))
+        .build()
+
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val view = inflater.inflate(R.layout.fragment_camera, container, false)
 
+        var label = requireActivity().application.assets.open("label.txt").bufferedReader().readLines()
+
+
         imageView = view.findViewById(R.id.imageView)
         pickImageButton = view.findViewById(R.id.pickImageButton)
+        scanButton = view.findViewById(R.id.scan)
 
         pickImageButton.setOnClickListener {
             checkStoragePermission()
         }
 
+
         viewModel.selectedImageUri.observe(viewLifecycleOwner) { uri ->
             imageView.setImageURI(uri)
+
+        }
+
+        scanButton.setOnClickListener{
+
+        viewModel.selectedImageBitmap.observe(viewLifecycleOwner){ bitmap ->
+        var tensorImage = TensorImage(DataType.FLOAT32)
+        tensorImage.load(bitmap)
+
+
+        tensorImage = imageProcessor.process(tensorImage)
+
+        val model = ImageliteModel.newInstance(requireContext())
+
+        val inputFeature0 = TensorBuffer.createFixedSize(intArrayOf(1, 224, 224, 3), DataType.FLOAT32)
+        inputFeature0.loadBuffer(tensorImage.buffer)
+
+        val outputs = model.process(inputFeature0)
+        val outputFeature0 = outputs.outputFeature0AsTensorBuffer.floatArray
+
+        var maxId = 0    
+        outputFeature0.forEachIndexed{index, fl ->  
+        if (outputFeature0[maxId] < fl){
+            maxId = index
+        }
+        }    
+
+         Toast.makeText(activity,label[maxId],Toast.LENGTH_SHORT).show()
+         model.close()
+         }
+
         }
 
         return view
     }
+
 
     private fun checkStoragePermission() {
         if (ContextCompat.checkSelfPermission(requireContext(), Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
@@ -87,16 +138,15 @@ class CameraFragment : Fragment() {
             when (requestCode) {
                 REQUEST_CODE_IMAGE_PICKER -> {
                     val imageUri: Uri? = data?.data
-                    imageUri?.let {
-                        viewModel.setImageUri(imageUri)
+                    val imageBitmap: Bitmap? = MediaStore.Images.Media.getBitmap(requireActivity().contentResolver,imageUri)
+                    imageUri.let {
+                        viewModel.setImageUri(imageUri!!)
+                    }
+                    imageBitmap.let {
+                        viewModel.setImageBitmap(imageBitmap!!)
                     }
                 }
             }
         }
-    }
-
-    fun classifyImage(bitmap: Bitmap){
-
-
     }
 }
--- app/src/main/java/com/example/capstoneproject/ui/camera/CameraViewModel.kt
+++ app/src/main/java/com/example/capstoneproject/ui/camera/CameraViewModel.kt
@@ -1,13 +1,18 @@
 package com.example.capstoneproject.ui.camera
 
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import android.net.Uri
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class CameraViewModel : ViewModel() {
     val selectedImageUri: MutableLiveData<Uri> = MutableLiveData()
-
+    val selectedImageBitmap: MutableLiveData<Bitmap> = MutableLiveData()
     fun setImageUri(uri: Uri) {
         selectedImageUri.value = uri
+    }
+    fun setImageBitmap(bitmap: Bitmap) {
+        selectedImageBitmap.value = bitmap
     }
 }
